5cf8069f03844cd477af9d4cf4a62da513dde8d4
josemaestri <maestri.jose@gmail.com>
2018-12-01 14:32:56 -0500

    init commit;basic structure

---
+++ app/data/data.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'link_item': [
+    {
+      'link_name': 'Github',
+      'link_url': 'https://github.com/josemaestri'
+    },
+    {
+      'link_name': 'Code Pen',
+      'link_url': 'https://codepen.io/josemaestri'
+    }
+  ] 
+}
